7a3e8c2091c6ecafca00318669fe1a3ab33a5aea
benchugg <ben.ih.chugg@gmail.com>
2016-06-04 19:23:45 -0700

    Redefine path urls in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,10 @@ Rails.application.routes.draw do
   # Define the root of the webpage
   root 'static_pages#home'
 
-  get 'static_pages/home'
-
-  get 'static_pages/getinvolved'
-
-  get 'static_pages/about'
-
-  get 'static_pages/events'
-
+  get 'about' => 'static_pages#about'
+  get 'getinvolved' => 'static_pages#getinvolved'
+  get 'events' => 'static_pages#events'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
