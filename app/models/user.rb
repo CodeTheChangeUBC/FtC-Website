855327d3f8edf172334d7adb50dcc1dec6404c38
benchugg <ben.ih.chugg@gmail.com>
2016-06-20 21:38:36 -0700

    Add functionality for users to edit their (basic) settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  # Here allow_nil: true for the password only takes effect when user is editing
+  # their account. This does not allow user to sign up with no password. 
 
   # Returns the hash digest of the given string. For testing user login/logout 
   # functionality. 
