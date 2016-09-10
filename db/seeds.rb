ecc323ec73adc0578aa086739c0a0cd68e952c07
benchugg <ben.ih.chugg@gmail.com>
2016-09-09 18:38:17 -0700

    cleanup for deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,93 +5,94 @@
 # Admin user
 User.create!(name:  "Admin",
              email: "admin@freethechildren.com",
-             password:              "foobar",
-             password_confirmation: "foobar", 
+             password:              "freethem!children",
+             password_confirmation: "freethem!children", 
              activated: true,
              activated_at: Time.zone.now,
              admin: true)
 
-9.times do |n|
-  name = "Executive #{n+1}"
-  email = "Executive#{n+1}@example.com"
-  password = "fake_password"
-  User.create!(name: name, 
-               email: email, 
-               password: password, 
-               password_confirmation: password,
-               about: "This is some stuff about me! " * 20,
-               activated: true,
-               activated_at: Time.zone.now, 
-               exec: true)
-end
-
-# Example user, non-admin. 
-User.create!(name:  "Example User",
-             email: "test@foobar.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             about: "This is some stuff about me! " * 20, 
-             activated: true,
-             activated_at: Time.zone.now)
-
-
-# For now, populate database with fake users. 
-50.times do |n|
- 		name  = Faker::Name.name
- 		email = "example-#{n+1}@foobar.org"
-		password = "password"
- 		User.create!(name:  name,
-       	       	 email: email,
-           	   	 password: password,
-            		 password_confirmation: password,
-                 about: "This is some stuff about me! " * 20, 
+if Rails.env.development?
+  9.times do |n|
+    name = "Executive #{n+1}"
+    email = "Executive#{n+1}@example.com"
+    password = "fake_password"
+    User.create!(name: name, 
+                 email: email, 
+                 password: password, 
+                 password_confirmation: password,
+                 about: "This is some stuff about me! " * 20,
                  activated: true,
-                 activated_at: Time.zone.now)
+                 activated_at: Time.zone.now, 
+                 exec: true)
+  end
+
+  # Example user, non-admin. 
+  User.create!(name:  "Example User",
+               email: "test@foobar.com",
+               password:              "foobar",
+               password_confirmation: "foobar",
+               about: "This is some stuff about me! " * 20, 
+               activated: true,
+               activated_at: Time.zone.now)
+
+
+  # For now, populate database with fake users. 
+  50.times do |n|
+   		name  = Faker::Name.name
+   		email = "example-#{n+1}@foobar.org"
+  		password = "password"
+   		User.create!(name:  name,
+         	       	 email: email,
+             	   	 password: password,
+              		 password_confirmation: password,
+                   about: "This is some stuff about me! " * 20, 
+                   activated: true,
+                   activated_at: Time.zone.now)
+  end
+
+  # For now, populate the database with fake documents
+
+  # Public documents
+  10.times do |n|
+    name = "Public Document #{n+1} (Fake, Will not open)"
+    attachment_url = "fake_url"
+    Document.create!(name: name, 
+                     attachment: attachment_url, 
+                     public: true)
+  end
+
+  # Private documents
+  10.times do |n|
+    name = "Member Only Document #{n+1} (Fake, Will not open)"
+    attachment_url = "fake_url"
+    Document.create!(name: name, 
+                     attachment: attachment_url)
+  end
+
+  # Articles
+  12.times do |n|
+    title = "Club Update #{n+1}"
+    text = "This is a club update. " * 50
+    Article.create!(title: title, text: text)
+  end
+
+  # Fundraiser
+  Fundraiser.create!(title: "Example Fundraiser", 
+                     description: "This is an example fundraiser" * 15, 
+                     progress: 362, 
+                     target: 500)
+
+  # Events
+  20.times do |n|
+    title = "Event #{n}"
+    description = "FTC Event."
+    start_time = Date.today + 2*n
+    Event.create!(title: title, 
+                  description: description, 
+                  start_time: start_time)
+
+  end
 end
-
-# For now, populate the database with fake documents
-
-# Public documents
-10.times do |n|
-  name = "Public Document #{n+1} (Fake, Will not open)"
-  attachment_url = "fake_url"
-  Document.create!(name: name, 
-                   attachment: attachment_url, 
-                   public: true)
-end
-
-# Private documents
-10.times do |n|
-  name = "Member Only Document #{n+1} (Fake, Will not open)"
-  attachment_url = "fake_url"
-  Document.create!(name: name, 
-                   attachment: attachment_url)
-end
-
-# Articles
-12.times do |n|
-  title = "Club Update #{n+1}"
-  text = "This is a club update. " * 50
-  Article.create!(title: title, text: text)
-end
-
-# Fundraiser
-Fundraiser.create!(title: "Example Fundraiser", 
-                   description: "This is an example fundraiser" * 15, 
-                   progress: 362, 
-                   target: 500)
-
-# Events
-20.times do |n|
-  title = "Event #{n}"
-  description = "FTC Event."
-  start_time = Date.today + 2*n
-  Event.create!(title: title, 
-                description: description, 
-                start_time: start_time)
-
-end
-
 
 
 
