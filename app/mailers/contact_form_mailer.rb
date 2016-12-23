23f59c94f3c6d84c859f6beaf55d4979b82f24fe
benchugg <ben.ih.chugg@gmail.com>
2016-12-22 18:33:18 -0800

    home page revamp

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -3,9 +3,8 @@ class ContactFormMailer < ApplicationMailer
 	def contact_form_email(message)
 		@message = message
 		@subject = "New message from #{@message[:name]}"
-		# Ben's email for testing purposes. Replace later with FTC's email. 
-		mail to: "ben.ih.chugg@gmail.com", 
-			 from: "noreply@freethechildren.com", 
+		mail to: "ubcfreethechildren@gmail.com", 
+			 from: "#{message[:email]}", 
 			 subject: @subject
 	end
 
