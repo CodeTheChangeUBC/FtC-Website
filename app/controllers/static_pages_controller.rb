23306b0b7efeff8a69f581d35cb1bef1c587eed6
benchugg <ben.ih.chugg@gmail.com>
2016-12-23 08:27:00 -0800

    minor styling changes to who we are and what we do pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,9 @@ class StaticPagesController < ApplicationController
   end
 
   def events
+    @vow_of_silence_event = Event.where(vow_silence: true).order(:created_at).first
+    @ee_challenge_event = Event.where(ee_challenge: true).order(:created_at).first
+    @mini_we_day_event = Event.where(we_day: true).order(:created_at).first
   end
 
   def contact
